52d431b50ab07b8afada39af181bf961216f3e20
sanjapriselac <sanja.priselac243@gmail.com>
2021-03-02 16:24:09 +0100

    quantlie normalisation and diff. privacy

--- DGE_snakemake/workflow/scripts/dgea.R
+++ DGE_snakemake/workflow/scripts/dgea.R
@@ -1,22 +1,38 @@
 #!/opt/conda/envs/R_env/bin/Rscript --vanilla
 # Sanja Priselac
+# 02/03/2021
 
 ##################
 library(ggplot2)
 library(limma)
+library(VennDiagram)
 ##################
 
-load(snakemake@input[[1]])
-load(snakemake@input[[2]])
-load(snakemake@input[[3]])
+for (i in 1:13) {
+  load(snakemake@input[[i]])
+}
 
 condition <- factor(paste(samples$treatment, samples$resistance, sep="."))
 design <- model.matrix(~ 0 + condition) 
 colnames(design) <- gsub("condition", "", colnames(design))
+
 ## Apply the limma-voom method:
-v <- voomWithQualityWeights(y, design, plot=T)
+v <- voomWithQualityWeights(y_qn, design, plot=T)
+
+v.05 <- voomWithQualityWeights(y_05, design, plot=T)
+v.1 <- voomWithQualityWeights(y_1, design, plot=T)
+v.25 <- voomWithQualityWeights(y_25, design, plot=T)
+v.5 <- voomWithQualityWeights(y_5, design, plot=T)
+v.10 <- voomWithQualityWeights(y_10, design, plot=T)
+
 
 fit <- lmFit(v)
+
+fit.05 <- lmFit(v.05)
+fit.1 <- lmFit(v.1)
+fit.25 <- lmFit(v.25)
+fit.5 <- lmFit(v.5)
+fit.10 <- lmFit(v.10)
 
 cont.matrix <- makeContrasts(
   treatmentInR  = Challenged.Resistant - Unchallenged.Resistant,
@@ -35,8 +51,45 @@ cont.matrix <- makeContrasts(
 
 fit2 <- contrasts.fit(fit, cont.matrix)
 fit2 <- eBayes(fit2)
+
+fit2.05 <- contrasts.fit(fit.05, cont.matrix)
+fit2.05 <- eBayes(fit2.05)
+fit2.1 <- contrasts.fit(fit.1, cont.matrix)
+fit2.1 <- eBayes(fit2.1)
+fit2.25 <- contrasts.fit(fit.25, cont.matrix)
+fit2.25 <- eBayes(fit2.25)
+fit2.5 <- contrasts.fit(fit.5, cont.matrix)
+fit2.5 <- eBayes(fit2.5)
+fit2.10 <- contrasts.fit(fit.10, cont.matrix)
+fit2.10 <- eBayes(fit2.10)
+
 results <- decideTests(fit2, p.value=0.1) 
-summary(results)
+
+results.05 <- decideTests(fit2.05, p.value=0.05) 
+results.1 <- decideTests(fit2.1, p.value=0.05) 
+results.25 <- decideTests(fit2.25, p.value=0.05) 
+results.5 <- decideTests(fit2.5, p.value=0.05) 
+results.10 <- decideTests(fit2.10, p.value=0.05) 
+
+genes.up <- list(qn = which(results@.Data[, 1]==1), dp0.5 = which(results.05@.Data[, 1]==1), dp1 = which(results.1@.Data[, 1]==1), 
+                 dp2.5 = which(results.25@.Data[, 1]==1), dp5 = which(results.5@.Data[, 1]==1), dp10 = which(results.10@.Data[, 1]==1))
+
+genes.down <- list(qn = which(results@.Data[, 1]==-1), dp0.5 = which(results.05@.Data[, 1]==-1), dp1 = which(results.1@.Data[, 1]==-1), 
+                   dp2.5 = which(results.25@.Data[, 1]==-1), dp5 = which(results.5@.Data[, 1]==-1), dp10 = which(results.10@.Data[, 1]==-1))
+
+## Venn diagrams
+venn.diagram(genes.up[1:4], filename = "/tmp/repo/DGE_snakemake/results/plots/venn.up1.png", 
+             fill = c("#999999", "#E69F00", "#56B4E9", "#009E73"), imagetype="png")
+venn.diagram(genes.up[c(1, 4:6)], filename = "/tmp/repo/DGE_snakemake/results/plots/venn.up2.png", 
+             fill = c("#999999", "#E69F00", "#56B4E9", "#009E73"), imagetype="png")
+venn.diagram(genes.down[1:4], filename = "/tmp/repo/DGE_snakemake/results/plots/venn.down1.png", 
+             fill = c("#999999", "#E69F00", "#56B4E9", "#009E73"), imagetype="png")
+venn.diagram(genes.down[c(1, 4:6)], filename = "/tmp/repo/DGE_snakemake/results/plots/venn.down2.png", 
+             fill = c("#999999", "#E69F00", "#56B4E9", "#009E73"), imagetype="png")
+
+###################################################################################################################
+## Volacno Plots
+###################################################################################################################
 
 # i corresponds to the coeff, meaning the contrasts
 volcano_plot <- function (i) {
@@ -74,19 +127,23 @@ ggsave(paste0("volcanoPlot_",names(fit2$contrasts[1,])[6], '.pdf'), plot=p6, pat
 # Extraction of differentially expressed genes
 ###################################
 
-## Treatment:
-treatmentInRGenes <- topTable(fit2, coef=1, number=Inf, sort.by="P")
-treatmentInSGenes <- topTable(fit2, coef=2, number=Inf, sort.by="P")
-treatmentGenes <- topTable(fit2, coef=3, number=Inf, sort.by="P") 
+fits <- list(fit2, fit2.05, fit2.1, fit2.25, fit2.5, fit2.10)
+for (i in 1:length(fits)) {
+  ## Treatment:
+  treatmentInRGenes <- topTable(fits[[i]], coef=1, number=Inf, sort.by="P")
+  treatmentInSGenes <- topTable(fits[[i]], coef=2, number=Inf, sort.by="P")
+  treatmentGenes <- topTable(fits[[i]], coef=3, number=Inf, sort.by="P") 
+  
+  ## Resistance:
+  resistanceInCGenes <- topTable(fits[[i]], coef=4, number=Inf, sort.by="P")
+  resistanceInUGenes <- topTable(fits[[i]], coef=5, number=Inf, sort.by="P")
+  resistanceGenes <- topTable(fits[[i]], coef=6, number=Inf, sort.by="P")
+  
+  write.csv(treatmentInRGenes, file=snakemake@output[[i]])
+  write.csv(treatmentInSGenes, file=snakemake@output[[6+i]])
+  write.csv(treatmentGenes, file=snakemake@output[[12+i]])
+  write.csv(resistanceInCGenes, file=snakemake@output[[18+i]])
+  write.csv(resistanceInUGenes, file=snakemake@output[[24+i]])
+  write.csv(resistanceGenes, file=snakemake@output[[30+i]])
+}
 
-## Resistance:
-resistanceInCGenes <- topTable(fit2, coef=4, number=Inf, sort.by="P")
-resistanceInUGenes <- topTable(fit2, coef=5, number=Inf, sort.by="P")
-resistanceGenes <- topTable(fit2, coef=6, number=Inf, sort.by="P")
-
-write.csv(treatmentInRGenes, file=snakemake@output[[1]])
-write.csv(treatmentInSGenes, file=snakemake@output[[2]])
-write.csv(treatmentGenes, file=snakemake@output[[3]])
-write.csv(resistanceInCGenes, file=snakemake@output[[4]])
-write.csv(resistanceInUGenes, file=snakemake@output[[5]])
-write.csv(resistanceGenes, file=snakemake@output[[6]])
--- DGE_snakemake/workflow/scripts/normalisation.R
+++ DGE_snakemake/workflow/scripts/normalisation.R
@@ -1,10 +1,12 @@
 #!/opt/conda/envs/R_env/bin/Rscript --vanilla
 # Sanja Priselac
+# 02/03/2021
 
 ########################
 library(edgeR)
 library(limma)
 library(ggplot2)
+library(rmutil)
 ########################
 
 load(snakemake@input[[1]])
@@ -15,35 +17,100 @@ load(snakemake@input[[2]])
 ############################################################################################
 
 y <- DGEList(txi$counts)
-y <- calcNormFactors(y, method="upperquartile")
-y$samples
 
-#myPalette <- c(brewer.pal(9, "Set1"), brewer.pal(8, "Set2"))
-## Plot CPM distribution (counts per million)
-#plotDensities(cpm(y), col=myPalette[1:16], legend=FALSE) #really skewed
-unfilteredExpr <- cpm(y, log=T)
+############################################################################################
+# Quantile normalisation function 
+############################################################################################
+
+quartileNormalisation <- function(y) {
+  ## y is DGEList object
+  # save the indices of the sort
+  orders <- apply(y$counts, 2, order, decreasing = TRUE)
+  x.sort <- apply(y$counts, 2, sort, decreasing = TRUE)
+  r.means <- rowMeans(x.sort)
+  for (i in 1:ncol(x.sort)) {
+    y$counts[orders[, i], i] <- r.means
+  }
+  return(y)
+} 
+
+############################################################################################
+# Laplace Quantile normalisation function 
+############################################################################################
+
+quartileNormalisation.dp <- function(y, b, seed=100) {
+  ## y is DGEList object
+  # save the indices of the sort
+  orders <- apply(y$counts, 2, order, decreasing = TRUE)
+  x.sort <- apply(y$counts, 2, sort, decreasing = TRUE)
+  r.means <- rowMeans(x.sort)
+  
+  ## laplace distributed random variables
+  set.seed(seed)
+  lap <- rlaplace(nrow(y$counts), m=0, s=b)
+  ## add the noise and sort the vector, L3 
+  r.means <- sort(r.means + lap, decreasing = TRUE)
+  negatives <- which(r.means < 0)
+  if (length(negatives) > 0) {
+    warning(cat(length(negatives), 'r.means values negative and set to 0'))
+    r.means[negatives] <- 0
+  }
+  for (i in 1:ncol(x.sort)) {
+    y$counts[orders[, i], i] <- r.means
+  }
+  return(y)
+} 
+
+
+############################################################################################
+
+
+y.qn <- quartileNormalisation(y)
+unfilteredExpr <- cpm(y.qn, log=T)
 #plotDensities(unfilteredExpr, col=myPalette[1:16], legend=FALSE)
 
 cutoff <- 0 
-summary(unfilteredExpr > cutoff)
+#summary(unfilteredExpr > cutoff)
 numSamplesWithExpression <- apply(unfilteredExpr, 1, function(x){ return(sum(x > cutoff)) })
-#hist(numSamplesWithExpression)
 
 ## Most genes are either expressed in all samples, or in no sample; retain all genes expressed in at least half of the samples:
 selectedGenes <- names(which(numSamplesWithExpression >= 8))
-length(selectedGenes) #8990
+#length(selectedGenes) #9021 
 
 # rebuild a new DGE object using only selected genes, and renormalize it
 y <- DGEList(txi$counts[selectedGenes, ]) 
-y <- calcNormFactors(y,  method="upperquartile")
-filteredExpr <- cpm(y, log=T)
-#plotDensities(filteredExpr, col=myPalette[1:16], legend=FALSE) #"topright"
 
-#plotDensities(filteredExpr, group=samples$resistance, col=myPalette[1:2], legend='topright')
-#plotDensities(filteredExpr, group=samples$treatment, col=myPalette[3:4], legend='topright')
+y_qn <- quartileNormalisation(y)
+## calculate laplacian 
+y_05 <- quartileNormalisation.dp(y, 0.5)
+y_1 <- quartileNormalisation.dp(y, 1)
+y_25 <- quartileNormalisation.dp(y, 2.5)
+y_5 <- quartileNormalisation.dp(y, 5)
+y_10 <- quartileNormalisation.dp(y, 10)
+
+filteredExpr <- cpm(y_qn, log=T)
+filteredExpr_05 <- cpm(y_05, log=T)
+filteredExpr_1 <- cpm(y_1, log=T)
+filteredExpr_25 <- cpm(y_25, log=T)
+filteredExpr_5 <- cpm(y_5, log=T)
+filteredExpr_10 <- cpm(y_10, log=T)
+
+save(y_qn, file=snakemake@output[[1]])
+save(y_05, file=snakemake@output[[2]])
+save(y_1, file=snakemake@output[[3]])
+save(y_25, file=snakemake@output[[4]])
+save(y_5, file=snakemake@output[[5]])
+save(y_10, file=snakemake@output[[6]])
+
+save(filteredExpr, file=snakemake@output[[7]])
+save(filteredExpr_05, file=snakemake@output[[8]])
+save(filteredExpr_1, file=snakemake@output[[9]])
+save(filteredExpr_25, file=snakemake@output[[10]])
+save(filteredExpr_5, file=snakemake@output[[11]])
+save(filteredExpr_10, file=snakemake@output[[12]])
 
 ############################################################################################
-# Data clustering
+# Data clustering, only on quartile normalized data
 ############################################################################################
 
 pca <- prcomp(t(filteredExpr), scale = T)
@@ -76,5 +143,3 @@ pc34 <- plotPCs(3, 4) # PC4 correlates with the resistance
 ggsave("PCA34.pdf", plot=pc34, path = "/tmp/repo/DGE_snakemake/results/plots")
 
 
-save(filteredExpr, file=snakemake@output[[1]])
-save(y, file=snakemake@output[[2]])
